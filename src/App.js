91257a4bf6f49362a3d839e5dfb5230cbeed265b
varunvj1 <varunvj181@gmail.com>
2020-05-01 16:53:23 +0530

    Form upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+// import logo from './logo.svg';
+import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+import { FacebookLoginButton } from "react-social-login-buttons";
+import "./App.css";
+import { from } from "rxjs";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Form className="login">
+        <h1 className="text-center mb-4">Welcome</h1>
+        <FormGroup>
+          <Label>Email</Label>
+          <Input type="email" placeholder="Email"></Input>
+        </FormGroup>
+        <FormGroup>
+          <Label>Password</Label>
+          <Input type="password" placeholder="Password"></Input>
+        </FormGroup>
+        <Button className="btn-block btn-large btn-dark"> Login </Button>
+        <div className="separator mt-3"> OR </div>
+        <FacebookLoginButton className="mt-3 mb-2" />
+        <div className="text-center">
+          <a href="#"> Sign up</a>
+          <span className="p-2"> | </span>
+          <a href="#"> Forgot password</a>
+        </div>
+      </Form>
+    );
+  }
 }
 
 export default App;
