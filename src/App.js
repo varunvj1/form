ae6220ce42e18ecbfd8118284f9daeeb3e75063b
Varun Jain <varunvj181@gmail.com>
2020-06-15 20:55:07 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -6,25 +6,27 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
+      <div className="form-box" style={{justifyContent:'center'}}>
       <Form className="login">
-        <h1 className="text-center mb-4">Welcome</h1>
+        <h1 className="text-center mb-4" style={{fontSize:'55px'}}>Welcome</h1>
         <FormGroup>
-          <Label>Email</Label>
+          <Label> <b>Email </b></Label>
           <Input type="email" placeholder="Email"></Input>
         </FormGroup>
         <FormGroup>
-          <Label>Password</Label>
+          <Label> <b>Password</b></Label>
           <Input type="password" placeholder="Password"></Input>
         </FormGroup>
         <Button className="btn-block btn-large btn-dark"> Login </Button>
         <div className="separator mt-3"> OR </div>
-        <FacebookLoginButton className="mt-3 mb-2" />
+        <FacebookLoginButton />
         <div className="text-center">
-          <a href="#"> Sign up</a>
+          <a href=""> Sign up</a>
           <span className="p-2"> | </span>
-          <a href="#"> Forgot password</a>
+          <a href=""> Forgot password</a>
         </div>
       </Form>
+      </div>
     );
   }
 }
