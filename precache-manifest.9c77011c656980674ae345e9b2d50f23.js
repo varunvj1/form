68f73f15c772d7d65b4acf0bf1bd752b8d412192
Varun Jain <varunvj181@gmail.com>
2020-06-20 05:25:28 +0530

    Updates

---
+++ precache-manifest.9c77011c656980674ae345e9b2d50f23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1be758d6078b3e4adf6622b8d5947e2",
+    "url": "/form/index.html"
+  },
+  {
+    "revision": "381007034eccc42061bc",
+    "url": "/form/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "d792b74441ef1cbafd3c",
+    "url": "/form/static/css/main.e5efac7c.chunk.css"
+  },
+  {
+    "revision": "381007034eccc42061bc",
+    "url": "/form/static/js/2.20288f24.chunk.js"
+  },
+  {
+    "revision": "0524daf8a2156d89615aff3b1b291e2a",
+    "url": "/form/static/js/2.20288f24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d792b74441ef1cbafd3c",
+    "url": "/form/static/js/main.dbc0e363.chunk.js"
+  },
+  {
+    "revision": "34677fe410f2968bcf7d",
+    "url": "/form/static/js/runtime-main.28228bc0.js"
+  }
+]);
